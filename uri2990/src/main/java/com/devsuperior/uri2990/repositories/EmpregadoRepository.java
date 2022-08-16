656338708fea9962c8ba46d3105156fc407e84f2
melck <melck_junior@hotmail.com>
2022-08-16 15:40:56 +0100

    URI2990

--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,29 @@
 package com.devsuperior.uri2990.repositories;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2990.entities.Empregado;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+    @Query(nativeQuery = true, value = "SELECT empregados.cpf, empregados.nome, departamento.dnome "
+            + "FROM empregados "
+            + "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero "
+            + "LEFT JOIN trabalha ON trabalha.cpf_emp = empregado.cpf "
+            + "WHERE trabalha.cpf_emp IS NULL "
+            + "ORDER BY empregados.cpf")
+    List<EmpregadoDeptProjection> search1();
+
+   @Query(value = "SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(obj.cpf, obj.nome, obj.departamento.dnome "
+            + "FROM Empregado obj "
+            + "INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero "
+            + "LEFT JOIN trabalha ON trabalha.cpf_emp = empregado.cpf "
+            + "WHERE trabalha.cpf_emp IS NULL "
+            + "ORDER BY empregados.cpf")
+    List<EmpregadoDeptDTO> search2();
 
 }
