3fca09e4717df96c5a93d180082bd5fd8fa5e74e
melck <melck_junior@hotmail.com>
2022-08-16 14:30:23 +0100

    URI2609

--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.uri2609.repositories;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2609.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT categories.name, SUM(products.amount) " +
+            "FROM categories " +
+            "INNER JOIN products ON products.id_categories = categories.id " +
+            "GROUP BY categories.name")
+    List<CategorySumProjection> search1();
+
+     @Query(nativeQuery = true, value = "SELECT new com.devsuperior.uri2609.dto.CategorySumDTO( obj.category.name, SUM(products.amount)) " +
+                "FROM Product obj " +
+                "GROUP BY obj.category.name")
+        List<CategorySumDTO> search2();
 
 }
