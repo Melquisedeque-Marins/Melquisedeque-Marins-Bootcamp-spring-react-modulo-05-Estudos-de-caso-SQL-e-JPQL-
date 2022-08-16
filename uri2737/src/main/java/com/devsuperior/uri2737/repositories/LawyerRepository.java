63910061202fb9b03129870a6c72dc6de5100021
melck <melck_junior@hotmail.com>
2022-08-16 15:00:15 +0100

    URI2737

--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,30 @@
 package com.devsuperior.uri2737.repositories;
 
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
 
+    @Query(nativeQuery = true, value = "(SELECT name, customers_number AS costumersNumber "
+            + "FROM lawyers "
+            + "WHERE customers_number = ("
+            + "SELECT MAX(customers_number) "
+            + "FROM lawyers"
+            + ")) "
+            + "UNION ALL "
+            + "(SELECT name, customers_number "
+            + "FROM lawyers "
+            + "WHERE customers_number = ( "
+            + "SELECT MIN(customers_number) "
+            + "FROM lawyers "
+            + ")) "
+            + "UNION ALL "
+            + "(SELECT 'Average', ROUND(AVG(customers_number), 0) "
+            + "FROM lawyers)")
+    List<LawyerMinProjection> search1();
 }
